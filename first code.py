8ea878de7a3545eb031c9ec0e36592ca31cf2053
reaper666 <57996364+novichok228@users.noreply.github.com>
2020-04-18 22:37:00 +0300

    Update first code.py

--- first code.py
+++ first code.py
@@ -2,20 +2,23 @@ import hashlib
 
 # by reaper666
 
-print("1: MD5")
-print("2: SHA1")
-print("3: SHA224")
-print("4: SHA256")
-print("5: SHA384")
-print("6: SHA512")
-print("7: Blake2b")
-print("8: Blake2s")
+print("1: MD5 ")
+print("2: SHA1 ")
+print("3: SHA224 ")
+print("4: SHA256 ")
+print("5: SHA384 ")
+print("6: SHA512 ")
+print("7: Blake2b ")
+print("8: Blake2s ")
+print()
 
-num = int(input("Select the appropriate option"))
+num = int(input("Select the appropriate option "))
+print()
 if 1 > num or num > 8:
     print("Error")
 else:
-    inp = input("OK, now enter the string whose hash you need")
+    inp = input("OK, now enter the string whose hash you need ")
+    print()
     if num == 1:
         print(hashlib.md5(inp.encode('utf-8')).hexdigest())
     elif num == 2:
